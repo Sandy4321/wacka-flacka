65b90905d84ad2f42cfd4e64799f6391495ff369
Mark Offerman <m.a.j.offerman@gmail.com>
2017-07-28 09:38:24 +0200

    Using pandas for checking

--- explore.py
+++ explore.py
@@ -1,13 +1,16 @@
-import os 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = 2
+#import os 
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf 
-
+#import tensorflow as tf 
+import pandas as pd 
 
 DATA_FILE = '~/Data/ipinyou/1458/train.log.txt'
+train = pd.read_csv(DATA_FILE, sep = "/t")
+print("The various axes of the dataframe are: ", train.axes)
 
+"""
+<<-------------------------------------------------------->>
+TO DO: create dataframe and convert to variables
+TO DO: save dataframe and convert to binary files ,perhaps test with heads
 
-
-
-
-
+"""
